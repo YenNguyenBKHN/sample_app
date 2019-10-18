a503600006a54da4e37a50e8d2a0c025bb10da09
Yen Nguyen <yennguyen26101998@gmail.com>
2019-10-18 17:40:02 +0700

    Chapter 6: Modeling Users

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = Settings.email_validate_regex
+  before_save{self.email = email.downcase}
+
+  validates :name, presence: true, length: {maximum: Settings.max_name}
+  validates :email, presence: true, length: {maximum: Settings.max_email},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+  validates :password, presence: true, length: {maximum: Settings.max_pass}
+
+  has_secure_password
+end
