817302f4c7285b1fc9a49e636987c5e2e8334e9a
YenNguyenBKHN <yennguyen26101998@gmail.com>
2019-10-29 15:59:51 +0700

    Chapter 11: Account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,18 +14,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def edit; end
-
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t "welcome"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = t "check_mail"
+      redirect_to root_url
     else
       render :new
     end
   end
+
+  def edit; end
 
   def update
     if @user.update user_params
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,11 +1,12 @@
 Rails.application.configure do
   config.cache_classes = false
+
   config.eager_load = false
+
   config.consider_all_requests_local = true
 
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -18,13 +19,35 @@ Rails.application.configure do
   end
 
   config.active_storage.service = :local
+
   config.action_mailer.raise_delivery_errors = false
+
   config.action_mailer.perform_caching = false
+
   config.active_support.deprecation = :log
+
   config.active_record.migration_error = :page_load
+
   config.active_record.verbose_query_logs = true
+
   config.assets.debug = true
+
   config.assets.quiet = true
+
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.hosts.clear
+
+  host = "localhost:3000"
+
+  config.action_mailer.default_url_options = { host: host, protocol: "http" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV["GMAIL"],
+    password: ENV["PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 end
